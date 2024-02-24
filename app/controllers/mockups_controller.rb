bf73f7af57a7e4576da1967ad6a8f3b9c0e89114
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2024-02-24 22:50:49 +0100

    Ubuntudream eduport

---
+++ app/controllers/mockups_controller.rb
@@ -0,0 +1,8 @@
+class MockupsController < ApplicationController
+  def test_a
+  end
+
+  def eduport_index
+    render layout: 'empty'
+  end
+end
---
+++ app/javascript/application.js
@@ -0,0 +1,17 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+
+// codice per il tooltip on hover
+//var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+//var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+//  return new bootstrap.Tooltip(tooltipTriggerEl)
+//})
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// codice per il popover on click
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
